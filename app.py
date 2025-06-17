52458f81df285a0f34812d8b9d9de3d4337e0a75
Sahm2903 <sahm2903@gmail.com>
2025-06-17 17:07:43 -0500

    app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/scraper',methods=['GET'])
+@app.route('/',methods=['GET'])
 def run_scraper():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
